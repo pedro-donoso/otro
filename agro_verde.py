3b2e1078edea722728357b6f9654fed344a493ae
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-10-10 18:15:36 -0300

    agrego datos iniciales

--- agro_verde.py
+++ agro_verde.py
@@ -2,3 +2,16 @@
 Sistema de Gestión de Pedidos - AgroVerde
 """
 
+# DATOS INICIALES
+productos_disponibles = ["Tomates", "Lechugas", "Zanahorias", "Brócoli", "Espinacas"]
+
+categorias = {"Verduras", "Hortalizas", "Tubérculos"}
+
+pedidos = {
+    "P001": {
+        "cliente": ("Juan Pérez", "8888-8888", "San José"), "productos": ["Tomates", "Lechugas"], "estado": "Pendiente"
+    }
+}
+
+contador_pedidos = 2
+
