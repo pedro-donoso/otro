874d3d872e58c91c3a2a2a692cbc07e3776b439e
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-10-17 20:40:27 -0300

    verifico archivo

--- tareas.py
+++ tareas.py
@@ -3,3 +3,14 @@ import os
 
 ARCHIVO = "tareas.json"
 
+def cargar_tareas():
+    """Carga las tareas desde el archivo."""
+    if os.path.exists(ARCHIVO):
+        try:
+            with open(Archivo, 'r') as f:
+                return json.load(f)
+        except:
+            return []
+        return []
+    
+    
