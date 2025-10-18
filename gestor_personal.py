a692658dd26d492998c4a9dcf169dc75b2bae460
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-10-18 13:21:58 -0300

    creo variables de diferentes tipos y estructuras de datos

---
+++ gestor_personal.py
@@ -0,0 +1,17 @@
+"""
+SISTEMA PYTHON SIMPLIFICADO
+Proyecto educativo con conceptos básicos de Python
+"""
+
+#VARIABLES
+nombre_sistema = "Mi Sistema Python"
+version = 1.0
+activo = True
+contador = 0
+
+# ESTRUCTURAS DE DATOS
+contactos = {}
+historial = []
+
+
+
